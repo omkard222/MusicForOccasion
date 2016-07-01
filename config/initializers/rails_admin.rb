1d9e14b8c75b7f6b7880a858afb1be7942dbcfd4
bhimasen <brautaray@enbake.com>
2016-07-01 15:51:54 +0530

    changes for show history

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -151,17 +151,17 @@ RailsAdmin.config do |config|
     configure :History do
       pretty_value do
         util = bindings[:object]
-        uu = bindings[:object].headline
+        #uu = bindings[:object].headline
         #ddd = util.user.created_at.strftime(%Y-%m-%d)
-        if true
-        %{<div class="blah">
-            <p>#{util.stage_name} created by account <a href="/admin/user/#{util.id}">Account</a> with email #{util.user.email} on #{util.user.created_at.strftime('%m/%d/%y')}</p>
-          </div >}.html_safe
+        if util.previous_account_mail.present?
+          %{<div class="history">
+            <p>#{util.stage_name} created by account <a href="/admin/user/#{util.id}">Account</a> with email #{util.previous_account_mail} on #{util.user.created_at.strftime('%d/%m/%y')}</p>
+            <p>#{util.stage_name} migrated to account <a href="/admin/user/#{util.id}">Account</a> with email #{util.user.email} on #{util.migration_date.strftime('%d/%m/%y')}</p>
+          </div >}.html_safe 
         else
-          %{<div class="blah">
+          %{<div class="history_back">
             <p>#{util.stage_name} created by account <a href="/admin/user/#{util.id}">Account</a> with email #{util.user.email} on #{util.user.created_at.strftime('%m/%d/%y')}</p>
-            <p>#{util.stage_name} created by account <a href="/admin/user/#{util.id}">Account</a> with email #{util.user.email} on #{util.user.created_at.strftime('%m/%d/%y')}</p>
-          </div >}.html_safe
+          </div >}.html_safe   
         end  
       end
       children_fields [:stage_name] # will be used for searching/filtering, first field will be used for sorting
