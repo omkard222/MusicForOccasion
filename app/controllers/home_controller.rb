230742ffce5510fb54fd179cb29e5f2e755680af
bhimasen <brautaray@enbake.com>
2016-07-06 17:03:33 +0530

    changes for production search issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -63,35 +63,36 @@ class HomeController < ApplicationController
   end
 
   def browse_musicians
-    if session[:stage_name].present? || session[:location].present? || session[:genres].present? || session[:instruments].present?
-      #raise params.inspect
-      params[:stage_name] = session[:stage_name] 
-      params[:location] = session[:location] 
-      params[:genres]= session[:genres]
-      params[:instruments] = session[:instruments]
-      @select2_form = {
-        instrument_id: [['', Instrument.order(:name).all.map { |i| ["#{i.name}", i.id] }]],
-        genre_id: [['', Genre.order(:name).all.map { |g| ["#{g.name}", g.id] }]]
-      }
-      @profiles = Profile.musician_has_services.joins(:user, :musician_genres)
-      @profiles = stage_name_filter(@profiles, params[:stage_name]) if params[:stage_name].present?
-      @profiles = genres_filter(@profiles, params[:genres]) if params[:genres].present?
-      @profiles = instruments_filter(@profiles, params[:instruments]) if params[:instruments].present?
-      @search_term = params[:location]
-      @profiles = location_filter(@profiles, @search_term) if @search_term.present?
-      @profiles = priority_ordering(@profiles, 50)
-      @stage_name = params[:stage_name]
-      @location = params[:location]
-      @genres = params[:genres].split(",") if params[:genres].present?
-      @instruments = params[:instruments].split(",") if params[:instruments].present?
-      # session[:stage_name] = ""
-      # session[:location] = ""
-      # session[:genres] = ""
-      # session[:instruments] = ""
-      session[:stage_name] = nil 
-      session[:location] = nil 
-      session[:genres] = nil 
-      session[:instruments] = nil 
+    if params[:back] == "ok"
+      if session[:stage_name].present? || session[:location].present? || session[:genres].present? || session[:instruments].present?
+        params[:stage_name] = session[:stage_name] 
+        params[:location] = session[:location] 
+        params[:genres]= session[:genres]
+        params[:instruments] = session[:instruments]
+        @select2_form = {
+          instrument_id: [['', Instrument.order(:name).all.map { |i| ["#{i.name}", i.id] }]],
+          genre_id: [['', Genre.order(:name).all.map { |g| ["#{g.name}", g.id] }]]
+        }
+        @profiles = Profile.musician_has_services.joins(:user, :musician_genres)
+        @profiles = stage_name_filter(@profiles, params[:stage_name]) if params[:stage_name].present?
+        @profiles = genres_filter(@profiles, params[:genres]) if params[:genres].present?
+        @profiles = instruments_filter(@profiles, params[:instruments]) if params[:instruments].present?
+        @search_term = params[:location]
+        @profiles = location_filter(@profiles, @search_term) if @search_term.present?
+        @profiles = priority_ordering(@profiles, 50)
+        @stage_name = params[:stage_name]
+        @location = params[:location]
+        @genres = params[:genres].split(",") if params[:genres].present?
+        @instruments = params[:instruments].split(",") if params[:instruments].present?
+        # session[:stage_name] = ""
+        # session[:location] = ""
+        # session[:genres] = ""
+        # session[:instruments] = ""
+        session[:stage_name] = nil 
+        session[:location] = nil 
+        session[:genres] = nil 
+        session[:instruments] = nil
+      end    
     else
       @profiles = Profile.musician_has_services
       @select2_form = {
