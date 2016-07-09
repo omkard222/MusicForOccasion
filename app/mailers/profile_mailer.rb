ec1bdfb92b51b8a005e1f144f1f1029264a8f57a
bhimasen <brautaray@enbake.com>
2016-07-09 18:28:45 +0530

    chnage for mailer

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -19,6 +19,16 @@ class ProfileMailer < ApplicationMailer
     mail(to: "rupinder.enbake@gmail.com", subject: "invitation from #{@profile.stage_name} to connect with gigbazaar")
   end
 
+  def invite_twitter_mail(profile, name, email)
+    @profile = profile
+    @slug = @profile.slug
+    @email = email
+    @name = name
+    @profile_name = profile.user.first_name 
+    #mail(to: email, subject: "invitation from #{@profile.stage_name} to connect with gigbazaar")
+    mail(to: "rupinder.enbake@gmail.com", subject: "invitation from #{@profile.stage_name} to connect with gigbazaar")
+  end
+
   def facebook_connect_success_user(profile, name, email)
   	@profile = profile
   	@email = email
@@ -58,7 +68,49 @@ class ProfileMailer < ApplicationMailer
     @profile_email = profile.user.email
     #mail(to: @profile_email, subject: "disconnected #{@name} with facebook successfully")
     mail(to: "rupinder.enbake@gmail.com", subject: "disconnected #{@name} with facebook successfully")
-  end  
+  end 
+
+  def twitter_connect_success_user(profile, name, email)
+    @profile = profile
+    @email = email
+    @name = name
+    @profile_name = profile.user.first_name
+    @slug = @profile.id 
+    #mail(to: email, subject: "connected #{@name} with facebook successfully")
+    mail(to: "rupinder.enbake@gmail.com", subject: "connected #{@name} with twitter successfully") 
+  end 
+  def twitter_connect_success_profile(profile, name, email)
+    @profile = profile
+    @email = email
+    @name = name
+    @slug = @profile.id
+    @profile_name = profile.user.first_name
+    @profile_email = profile.user.email 
+    #mail(to: @profile_email, subject: "connected #{@name} with facebook successfully")
+    mail(to: "rupinder.enbake@gmail.com", subject: "connected #{@name} with twitter successfully") 
+  end
+
+  def twitter_disconnect_success_user(profile, name, email)
+    @profile = profile
+    @email = email
+    @name = name
+    @slug = @profile.id
+    @profile_name = profile.user.first_name 
+    #mail(to: @email, subject: "disconnected #{@name} with facebook successfully")
+    mail(to: "rupinder.enbake@gmail.com", subject: "disconnected #{@name} with twitter successfully")
+  end 
+
+  def twitter_disconnect_success_profile(profile, name, email)
+    @profile = profile
+    @email = email
+    @name = name
+    @slug = @profile.id
+    @profile_name = profile.user.first_name
+    @profile_email = profile.user.email
+    #mail(to: @profile_email, subject: "disconnected #{@name} with facebook successfully")
+    mail(to: "rupinder.enbake@gmail.com", subject: "disconnected #{@name} with twitter successfully")
+  end 
+ 
    
    def profile_mail_previous(profile, old_user, new_user)
     @profile = profile
