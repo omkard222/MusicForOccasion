4ab95425abcf3081d01f6f2054561d0a6f5ae5da
bhimasen <brautaray@enbake.com>
2016-07-14 16:53:10 +0530

    twitter disconnect

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
   include ServicesHelper
   skip_before_filter :verify_authenticity_token, :only => [:facebook_friend_page_connect, :facebook_friend_page, :invite_friend, :user_email_change, :invite_twitter_friend]
 
-  before_action :authenticate_user!, except: [:facebook_friend_page_connect, :show,:facebook_friend_page, :show_slug, :new, :create, :paypal_confirmation, :invite_friend, :user_email_change, :facebook_disconnect_friend]
+  before_action :authenticate_user!, except: [:facebook_friend_page_connect, :show,:facebook_friend_page, :show_slug, :new, :create, :paypal_confirmation, :invite_friend, :user_email_change, :facebook_disconnect_friend, :twitter_disconnect_friend]
   #before_action :verify_user, only: [:edit, :update, :delete]
   before_action :verify_user, only: [:update, :delete]
   before_action :check_destroy_profile_possibility, only: [:delete]
