f74cb6d335f14cc02a4e88cc65b3d5813c4852f8
bhimasen <brautaray@enbake.com>
2016-08-02 10:44:02 +0530

    deleted files

--- public/assets/facebook-10d463e5cb368c80e307dceb7f0dc0a67f6a7bb8d70b5b999bc881d25c778483.js
+++
@@ -1,1 +0,0 @@
-window.fbAsyncInit=function(){FB.init({appId:"#{ENV['FACEBOOK_APP_ID']}",xfbml:!0,version:"v2.2"})},function(e,n,t){var o,c=e.getElementsByTagName(n)[0];e.getElementById(t)||(o=e.createElement(n),o.id=t,o.src="//connect.facebook.net/en_US/sdk.js",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
--- public/assets/submenu-ad4df23f36c42af786eae4a2db7a80b5714a909754f45c2efc1c37c2598a003a.js
+++
@@ -1,1 +0,0 @@
-$("a#payment_method").on("click",function(e){$("div.content").load(this.href),e.preventDefault()}),$("a#receiving_money").on("click",function(e){$("div.content").load(this.href),e.preventDefault()});
