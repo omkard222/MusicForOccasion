5652b28cafde1b1acb363c8ef87d0fc7c4b8293a
= <=>
2016-08-12 19:26:11 +0530

    change settings for booker profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,13 +29,15 @@ module Users
             resource.profiles.create(profile_type: :registered_user,
                                      location: params[:user][:profile][:location],
                                      username: username,
-                                     stage_name: username)
+                                     stage_name: username,
+                                     sub_type: :default)
 
             set_flash_message :success, :signed_up_success if is_flashing_format?
             session[:is_musician] = nil
             if session[:try_book]
               profile_id = session[:try_book]
               session[:try_book] = nil
+             # redirect_to profile_type_profiles_path(type: 'registered_user', edit: false) 
               redirect_to profile_path(profile_id)
             else
               redirect_to root_path
